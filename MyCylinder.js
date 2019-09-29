506e45481b1ac468744d0c3e4a824dbecf87ab74
Goncalo101 <goncalo.oli@outlook.com>
2019-09-29 15:39:20 +0100

    Cylinders can now have different radii on the top and bottom

--- MyCylinder.js
+++ MyCylinder.js
@@ -25,11 +25,13 @@ class MyCylinder extends CGFobject {
         
         var alphaAng = 2 * Math.PI / this.slices;
         var stack_height = this.height / this.stacks;
+        var radius_variation = (this.radius_top - this.radius_bottom) / this.stacks;
 
-        for (var current_height = 0, index = 0; current_height <= this.height; current_height += stack_height) {
+        for (var current_height = 0, index = 0, radius = this.radius_bottom; current_height <= this.height; current_height += stack_height, radius += radius_variation) {
+            console.log("radius: " + radius);
             for (var current_angle = 0; current_angle < 2 * Math.PI; current_angle += alphaAng) {
-                var x_coord = Math.cos(current_angle);
-                var y_coord = Math.sin(current_angle);
+                var x_coord = radius * Math.cos(current_angle);
+                var y_coord = radius * Math.sin(current_angle);
                 var z_coord = current_height;
 
                 this.vertices.push(x_coord, y_coord, z_coord);
@@ -44,10 +46,10 @@ class MyCylinder extends CGFobject {
             slice_indices.push(temp);
         }
 
-        for (var stack = 0; stack < this.stacks; ++stack) {
+        for (var stack = 0; stack < this.stacks; stack++) {
             for (var i = 0; i < this.slices; i++) {
                 this.indices.push(slice_indices[stack][i], slice_indices[stack][i + 1], slice_indices[stack + 1][i]);
-                this.indices.push(slice_indices[stack][i + 1], slice_indices[stack + 1][i + 1], slice_indices[stack + 1][i])
+                this.indices.push(slice_indices[stack][i + 1], slice_indices[stack + 1][i + 1], slice_indices[stack + 1][i]);
             }
 
             this.indices.push(slice_indices[stack][this.slices - 1], slice_indices[stack][0], slice_indices[stack + 1][this.slices - 1]);
