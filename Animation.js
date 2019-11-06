a9d2bf2e79bb3f4293e5fd31fb496a964b4c4871
Goncalo101 <goncalo.oli@outlook.com>
2019-11-06 08:39:36 +0000

    Added animation stubs

---
+++ Animation.js
@@ -0,0 +1,12 @@
+/**
+ * class defining an abstract animation, including update and apply methods
+ */
+class Animation {
+    update(t) {
+
+    }
+
+    apply() {
+
+    }
+}
---
+++ KeyframeAnimation.js
@@ -0,0 +1,6 @@
+/**
+ * class defining an animation using keyframes
+ */
+class KeyframeAnimation extends Animation {
+
+}
