82a94cef85e9f28bfeddd427f35eb1b2334594cf
tiagocv <tcverdade@hotmail.com>
2019-11-20 09:59:47 +0000

    Fix cylinder form

--- MyCylinderNURBS.js
+++ MyCylinderNURBS.js
@@ -13,7 +13,7 @@ class MyCylinderNURBS extends CGFobject {
         this.slices = slices;
         this.stacks = stacks;
 
-        this.cyl = new CGFnurbsSurface(8, // degree on U: 2 control vertexes U
+        this.cyl1 = new CGFnurbsSurface(2, // degree on U: 2 control vertexes U
             1, // degree on V: 2 control vertexes on V
            [	// U = 0
                [ // V = 0..1;
@@ -30,49 +30,87 @@ class MyCylinderNURBS extends CGFobject {
                     [0.0*this.radius_bottom, -1.0*this.radius_bottom, 0.0*this.height, 1 ],
                     [0.0*this.radius_top, -1.0*this.radius_top, this.height, 1 ],
                ],
-            //    // U = 3
+            ]);
+
+        this.cyl2 = new CGFnurbsSurface(2, // degree on U: 2 control vertexes U
+            1, // degree on V: 2 control vertexes on V
+            [	// U = 0
+                [ // V = 0..1;
+                    [0.0*this.radius_bottom, -1.0*this.radius_bottom, 0.0*this.height, 1 ],
+                    [0.0*this.radius_top, -1.0*this.radius_top, this.height, 1 ],
+                ],
+                // U = 1
                 [ // V = 0..1;
                     [1.0*this.radius_bottom, -1.0*this.radius_bottom, 0.0*this.height, 0.707 ],
                     [1.0*this.radius_top, -1.0*this.radius_top, this.height, 0.707 ],
                 ],
-               //    // U = 4
-               [ // V = 0..1;
-                [1.0*this.radius_bottom, 0.0*this.radius_bottom, 0.0*this.height, 1 ],
-                [1.0*this.radius_top, 0.0*this.radius_top, this.height, 1 ],
+                // U = 2
+                [ // V = 0..1;
+                    [1.0*this.radius_bottom, 0.0*this.radius_bottom, 0.0*this.height, 1 ],
+                    [1.0*this.radius_top, 0.0*this.radius_top, this.height, 1 ],
                 ],
-                // U = 5
+            ]);
+
+        this.cyl3 = new CGFnurbsSurface(2, // degree on U: 2 control vertexes U
+            1, // degree on V: 2 control vertexes on V
+            [	// U = 0
+                [ // V = 0..1;
+                    [1.0*this.radius_bottom, 0.0*this.radius_bottom, 0.0*this.height, 1 ],
+                    [1.0*this.radius_top, 0.0*this.radius_top, this.height, 1 ],
+                ],
+                // U = 1
                 [ // V = 0..1;
                     [1.0*this.radius_bottom, 1.0*this.radius_bottom, 0.0*this.height, 0.707 ],
                     [1.0*this.radius_top, 1.0*this.radius_top, this.height, 0.707 ],
                 ],
-                //    // U = 4
-               [ // V = 0..1;
-                [0.0*this.radius_bottom, 1.0*this.radius_bottom, 0.0*this.height, 1 ],
-                [0.0*this.radius_top, 1.0*this.radius_top, this.height, 1 ],
+                // U = 2
+                [ // V = 0..1;
+                    [0.0*this.radius_bottom, 1.0*this.radius_bottom, 0.0*this.height, 1 ],
+                    [0.0*this.radius_top, 1.0*this.radius_top, this.height, 1 ],
                 ],
+            ]);
+
+        this.cyl4 = new CGFnurbsSurface(2, // degree on U: 2 control vertexes U
+            1, // degree on V: 2 control vertexes on V
+            [	// U = 0
+                [ // V = 0..1;
+                    [0.0*this.radius_bottom, 1.0*this.radius_bottom, 0.0*this.height, 1 ],
+                    [0.0*this.radius_top, 1.0*this.radius_top, this.height, 1 ],
+                ],
+                // U = 1
                 [ // V = 0..1;
                     [-1.0*this.radius_bottom, 1.0*this.radius_bottom, 0.0*this.height, 0.707 ],
                     [-1.0*this.radius_top, 1.0*this.radius_top, this.height, 0.707 ],
                 ],
+                // U = 2
                 [ // V = 0..1;
                     [-1.0*this.radius_bottom, 0.0*this.radius_bottom, 0.0*this.height, 1 ],
                     [-1.0*this.radius_top, 0.0*this.radius_top, this.height, 1 ],
                 ],
-           ]);
+            ]);
 
 
-        this.cylObj = new CGFnurbsObject(this.scene, this.slices, this.stacks, this.cyl); 
+        this.cylObj1 = new CGFnurbsObject(this.scene, this.slices, this.stacks, this.cyl1); 
+        this.cylObj2 = new CGFnurbsObject(this.scene, this.slices, this.stacks, this.cyl2); 
+        this.cylObj3 = new CGFnurbsObject(this.scene, this.slices, this.stacks, this.cyl3); 
+        this.cylObj4 = new CGFnurbsObject(this.scene, this.slices, this.stacks, this.cyl4); 
 
         this.initBuffers();
 
     }
 
     initBuffers(){
-        this.cylObj.initBuffers();
+        this.cylObj1.initBuffers();
+        this.cylObj2.initBuffers();
+        this.cylObj3.initBuffers();
+        this.cylObj4.initBuffers();
     }
 
     display(){
-        this.cylObj.display();
+        this.cylObj1.display();
+        this.cylObj2.display();
+        this.cylObj3.display();
+        this.cylObj4.display();
     }
 
     updateTexCoords(s_length, t_length) {
