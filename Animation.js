29b0b566b432e38dab83e081a937ec54e67f0dfc
Gonçalo Oliveira <goncalo.oli@outlook.com>
2019-11-23 16:19:28 +0000

    Translation (only) animations are working

--- Animation.js
+++ Animation.js
@@ -2,9 +2,7 @@
  * class defining an abstract animation, including update and apply methods
  */
 class Animation {
-    constructor(scene) {
-        this.animation_mat = mat4.create();
-    }
+    constructor(scene) {}
 
     update(t) {}
 
--- KeyframeAnimation.js
+++ KeyframeAnimation.js
@@ -5,8 +5,48 @@ class KeyframeAnimation extends Animation {
     constructor(scene, transformations) {
         super(scene);
         this.scene = scene;
-        this.transformations = transformations;
+
+        // process keyframe info 
+        // each element of this.keyframes is a list of 4 elements: the starting instant of the keyframe, 
+        // followed by the translation, rotation and scale vectors
+        this.keyframes = [[0, [0, 0, 0], [0, 0, 0], [1, 1, 1]]];
+        this.keyframes.push(transformations);
+
+        this.nkeyframes = this.keyframes.length; // number of keyframes (including keyframe 0)
+        this.keyframe_instants = [];
+        this.keyframe_animations = [];
+        this.curr_keyframe = 0;
+
+        // get starting instants of each keyframe
+        this.keyframes.forEach((transformation) => {
+            this.keyframe_instants.push(transformation[0]);
+            this.keyframe_animations = transformation.slice(1, 4);
+        })
+        
         console.log("keyframe transformations");
-        console.log(transformations);
+        console.log(this.keyframes);
+        console.log(this.keyframe_instants);
+        console.log(this.keyframe_animations);
+    }
+
+    update(t) {
+        var matrix = mat4.create();
+        var translate = vec3.create();
+        var curr_transfs = this.keyframe_animations;
+        var keyframe_time = this.keyframe_instants[this.curr_keyframe + 1] - this.keyframe_instants[this.curr_keyframe];
+        var real_time = t / 1000;
+        var lerp_factor;
+
+        if (real_time > keyframe_time)
+            lerp_factor = 1;
+        else 
+            lerp_factor = real_time * (1 / keyframe_time);
+
+        vec3.lerp(translate, translate, curr_transfs[0], lerp_factor);
+        mat4.translate(matrix, matrix, translate);
+
+        // console.log(this.matrix);
+
+        return matrix;
     }
 }
