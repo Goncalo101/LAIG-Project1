a93c1ecd0674f2a52bea36a6f198e7c90373ca3e
Gonçalo Oliveira <goncalo.oli@outlook.com>
2019-11-23 19:21:31 +0000

    Added scale transformation to keyframe animation handling

--- KeyframeAnimation.js
+++ KeyframeAnimation.js
@@ -21,12 +21,7 @@ class KeyframeAnimation extends Animation {
         this.keyframes.forEach((transformation) => {
             this.keyframe_instants.push(transformation[0]);
             this.keyframe_animations = transformation.slice(1, 4);
-        })
-        
-        console.log("keyframe transformations");
-        console.log(this.keyframes);
-        console.log(this.keyframe_instants);
-        console.log(this.keyframe_animations);
+        })        
     }
 
     update(t) {
@@ -34,26 +29,32 @@ class KeyframeAnimation extends Animation {
 
         var translation = vec3.create();
         var rotation = vec3.create();
+        var scale = vec3.create();
 
         var curr_transfs = this.keyframe_animations;
         var keyframe_time = this.keyframe_instants[this.curr_keyframe + 1] - this.keyframe_instants[this.curr_keyframe];
         var real_time = t / 1000;
         var lerp_factor;
 
+        // when keyframe ends lerp_factor should be 1 to prevent further animations
         if (real_time > keyframe_time)
             lerp_factor = 1;
         else 
             lerp_factor = real_time * (1 / keyframe_time);
 
+        // interpolate translation vector
         vec3.lerp(translation, translation, curr_transfs[0], lerp_factor);
         mat4.translate(matrix, matrix, translation);
         
+        // interpolate rotation
         vec3.lerp(rotation, rotation, curr_transfs[1], lerp_factor);
         mat4.rotate(matrix, matrix, rotation[0] * Math.PI / 180, [1,0,0]);
         mat4.rotate(matrix, matrix, rotation[1] * Math.PI / 180, [0,1,0]);
         mat4.rotate(matrix, matrix, rotation[2] * Math.PI / 180, [0,0,1]);
 
-        // console.log(this.matrix);
+        // interpolate scale
+        vec3.lerp(scale, scale, curr_transfs[2], lerp_factor);
+        mat4.scale(matrix, matrix, scale);
 
         return matrix;
     }
