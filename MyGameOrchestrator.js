699f82861561c98d5ac3446f6bd415e130e4a2cd
Goncalo101 <goncalo.oli@outlook.com>
2019-12-11 10:27:51 +0000

    Added picking handlers in orchestrator

--- MyGameOrchestrator.js
+++ MyGameOrchestrator.js
@@ -14,4 +14,28 @@ class MyGameOrchestrator {
         // this.gameboard.display()
         // this.animator.display()
     }
+
+    // These functions may not work out of the box, needs adaptation to our game
+    onObjectSelected(obj, id) {
+        if (obj instanceof MyPiece) {// do something with id knowing it is a piece
+        } else if (obj instanceof MyTile) {// do something with id knowing it is a tile
+        } else {// error ? 
+        }
+    }
+
+    managePick(mode, results) {
+        if (mode == false /* && some other game conditions */)
+            if (results != null && results.length > 0) { // any results?
+                for (var i = 0; i < results.length; i++) {
+                    var obj = pickResults[i][0]; // get object from result
+                    if (obj) { // exists?
+                        var uniqueId = pickResults[i][1] // get id
+                        this.onObjectSelected(obj, uniqueId);
+                    }
+                } 
+                
+                // clear results
+                pickResults.splice(0, pickResults.length);
+            }
+    }
 }
